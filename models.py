d29a8d87c88d6a44c5e7ac936658eaf47f74a3d3
David Maseda Neira <davidmaseda@taiba>
2019-10-22 12:09:49 +0200

    Spectral model

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from attention import SelfAttnModel
-from spectral import SpectralConv2D
+from spectral import SpectralConv2D,SpectralConv2DTranspose
 OUTPUT_CHANNELS = 2
 
 def downsample(filters,size,batchnorm = True):
@@ -22,7 +22,7 @@ def upsample(filters,size,dropout = False):
 
     result = tf.keras.Sequential()
 
-    result.add(tf.keras.layers.Conv2DTranspose(filters,size,strides=2,padding='same',use_bias=False))
+    result.add(SpectralConv2DTranspose(filters,size,strides=2,padding='same',use_bias=False))
     result.add(tf.keras.layers.BatchNormalization())
 
     if dropout:
@@ -33,38 +33,74 @@ def upsample(filters,size,dropout = False):
     return result
 
 def Generator():
-        '''
-        conditioning = Input(shape=(None,None,self.G_INPUT_CHANNELS))
-        input_layer = Input(shape=(None,None,1))
-        filters = 32
-        c1 = self.addConvStep(input_layer,filters,conditioning)
-        filters = 32
-        c2 = self.addConvStep(c1,filters,conditioning)
-        filters = 16
-        c3 = self.addConvStep(c2,filters,conditioning)
-        filters = 2
-        c4 = SeparableConv2D(filters,self.KERNEL_SIZE,padding='same',activation=customAct)(c3)
-        model = Model([input_layer,conditioning],c4)
-        print("--Generator--")
-        model.summary()
-        '''
-        init = tf.random_normal_initializer(0.,0.02)
-        conditioning = tf.keras.layers.Input(shape=(256, 256, 1))
-        #hid,att2 = SelfAttnModel(1)(conditioning)
-        #noise = tf.keras.layers.Input(shape=(None,None,1))
-        #hid = tf.keras.layers.Concatenate()([noise,conditioning])
-        hid = SpectralConv2D(2, (3, 3), activation='relu', padding='same')(conditioning)
-        hid = tf.keras.layers.BatchNormalization()(hid)
-        hid = SpectralConv2D(16, (3, 3), activation='relu', padding='same')(hid)
-        hid = tf.keras.layers.BatchNormalization()(hid)
-        hid = SpectralConv2D(16, (3, 3), activation='relu', padding='same')(hid)
-        hid = SpectralConv2D(16, (3, 3), activation='relu', padding='same')(hid)
-        #hid,att1 = SelfAttnModel(16)(hid)
-        hid = SpectralConv2D(2, (3, 3)  ,activation='relu', padding='same')(hid)
-        hid =  SpectralConv2D(2, (3, 3), activation='tanh',padding='same')(hid)
-        model = tf.keras.Model(conditioning,hid)
-        
-        return model
+    down_stack = [
+        downsample(64, 4, batchnorm=False), # (bs, 128, 128, 64)
+        downsample(128, 4), # (bs, 64, 64, 128)
+        downsample(256, 4), # (bs, 32, 32, 256)
+        downsample(512, 4), # (bs, 16, 16, 512)
+        downsample(512, 4), # (bs, 8, 8, 512)
+        downsample(512, 4), # (bs, 4, 4, 512)
+        downsample(512, 4), # (bs, 2, 2, 512)
+        downsample(512, 4), # (bs, 1, 1, 512)
+    ]
+
+    up_stack = [
+        upsample(512, 4, dropout=True), # (bs, 2, 2, 1024)
+        upsample(512, 4, dropout=True), # (bs, 4, 4, 1024)
+        upsample(512, 4, dropout=True), # (bs, 8, 8, 1024)
+        upsample(512, 4), # (bs, 16, 16, 1024)
+        upsample(256, 4), # (bs, 32, 32, 512)
+        upsample(128, 4), # (bs, 64, 64, 256)
+        upsample(64, 4), # (bs, 128, 128, 128)
+    ]
+
+    initializer = tf.random_normal_initializer(0., 0.02)
+    last = tf.keras.layers.Conv2DTranspose(OUTPUT_CHANNELS, 4,
+                                         strides=2,
+                                         padding='same',
+                                         kernel_initializer=initializer,
+                                         activation='tanh') # (bs, 256, 256, 3)
+
+    concat = tf.keras.layers.Concatenate()
+
+    inputs = tf.keras.layers.Input(shape=[None,None,1])
+    x = inputs
+    # Downsampling through the model
+    skips = []
+    for down in down_stack:
+        x = down(x)
+        skips.append(x)
+
+    skips = reversed(skips[:-1])
+
+    # Upsampling and establishing the skip connections
+    for up, skip in zip(up_stack, skips):
+        x = up(x)
+        x = concat([x, skip])
+
+    x = last(x)
+
+    return tf.keras.Model(inputs=inputs, outputs=x)
+
+    '''
+    init = tf.random_normal_initializer(0.,0.02)
+    conditioning = tf.keras.layers.Input(shape=(256, 256, 1))
+    #hid,att2 = SelfAttnModel(1)(conditioning)
+    #noise = tf.keras.layers.Input(shape=(None,None,1))
+    #hid = tf.keras.layers.Concatenate()([noise,conditioning])
+    hid = SpectralConv2D(2, (3, 3), activation='relu', padding='same')(conditioning)
+    hid = tf.keras.layers.BatchNormalization()(hid)
+    hid = SpectralConv2D(16, (3, 3), activation='relu', padding='same')(hid)
+    hid = tf.keras.layers.BatchNormalization()(hid)
+    hid = SpectralConv2D(16, (3, 3), activation='relu', padding='same')(hid)
+    hid = SpectralConv2D(16, (3, 3), activation='relu', padding='same')(hid)
+    #hid,att1 = SelfAttnModel(16)(hid)
+    hid = SpectralConv2D(2, (3, 3)  ,activation='relu', padding='same')(hid)
+    hid =  SpectralConv2D(2, (3, 3), activation='tanh',padding='same')(hid)
+    model = tf.keras.Model(conditioning,hid)
+    
+    return model
+    '''
 
 
 
